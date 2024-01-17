b1fea9d05da204f7d6a4af8fdb399d7e5d145517
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-01-17 08:08:56 +0530

    Time: 71 ms (22.19%), Space: 55.3 MB (76.40%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,8 +1,58 @@
 class Solution {
     public int[] sortArray(int[] nums) {
      
+   
+        mergeSort(nums,0,nums.length-1);
+         return nums;
+    }
+    
+    void mergeSort(int []nums,int low ,int high){
         
-        Arrays.sort(nums);
-        return nums;
+         if(low >= high) return;
+        int mid= (high +low)/2;
+        
+       
+        mergeSort(nums,low , mid);
+        mergeSort(nums,mid+1, high);
+        merge(nums,low,mid,high);
+         
+    }
+    
+    void merge(int nums[], int low,int mid,int high){
+        ArrayList<Integer> temp= new ArrayList<>();
+        
+        int left =low;
+        int right =mid +1;
+        
+        while(left <= mid && right <= high){
+            if(nums[left]<= nums[right]){
+                temp.add(nums[left]);
+                left++;
+            }else{
+                temp.add(nums[right]);
+                right++;
+            }
+        }
+        
+        
+          while (left <= mid) {
+            temp.add(nums[left]);
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(nums[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp.get(i - low);
+        }
+        
+        
+        
+        
     }
 }
