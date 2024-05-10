c0244f7277f9c0f6607762aeaab7368a8f1de958
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-05-10 18:22:25 +0530

    Time: 1 ms (95.06%), Space: 44.4 MB (57.68%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds =  new ArrayList<>();
+        boolean isVis[] = new boolean[nums.length];
+        findPermutations(nums , ans , ds , isVis );
+        return ans;
+    }
+    void findPermutations(int nums[] , List<List<Integer>> res , List<Integer> ds , boolean[] isVis){
+          if(ds.size() == nums.length){
+            res.add(new ArrayList<>(ds));
+            return;
+          }
+          for(int i =0;i<nums.length;i++){
+            if(!isVis[i]){
+                isVis[i]= true;
+                ds.add(nums[i]);
+                findPermutations(nums, res, ds, isVis);
+                ds.remove(ds.size()-1);
+                isVis[i]= false;
+            }
+          }
+    }
+}
