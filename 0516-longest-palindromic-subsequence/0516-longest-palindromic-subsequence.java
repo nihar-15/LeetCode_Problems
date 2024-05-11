9c503d8c4aa142335a70ba04b6dc386bfba4b8dc
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-05-11 15:22:04 +0530

    Time: 41 ms (59.54%), Space: 55.6 MB (8.37%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+         String reversed = new StringBuilder(s).reverse().toString();
+        
+      return lcs(s, reversed);
+    }
+    public int lcs(String text1 , String text2){
+          int n = text1.length();
+     int m = text2.length();
+     int dp[][] = new int[n][m];
+     for(int a[] :dp){
+        Arrays.fill(a , -1);
+     }
+    return  func(text1 , text2 , n-1, m-1, dp);
+
+        }
+        int func(String text1 , String text2, int n , int m, int dp[][]){
+            if(n <0 || m<0){
+                return 0;
+            }
+            if(dp[n][m] != -1){
+                return dp[n][m];
+            }
+            if(text1.charAt(n) == text2.charAt(m)){
+                return dp[n][m] = 1+ func(text1 , text2 , n-1 , m-1 , dp);
+            }
+            return dp[n][m]= 0 + Math.max(func(text1 , text2 ,n-1 , m ,dp) , func(text1 , text2 , n , m-1 , dp));
+        }
+    
+}
