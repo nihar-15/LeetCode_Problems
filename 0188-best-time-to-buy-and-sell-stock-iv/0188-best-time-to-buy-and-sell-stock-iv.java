133d4e6484ecc40c44a1527e34c3a6ab8b6efd2e
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-05-18 13:47:30 +0530

    Time: 5 ms (45.73%), Space: 43.2 MB (52.49%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+          int dp[][][] = new int[2][k+1][prices.length];
+        for(int [][]d: dp){
+            for(int a[] : d){
+            Arrays.fill( a , -1);
+        }
+        }
+      return func(prices , 0, 1 , k, dp);
+    }
+    int func(int prices[] , int day , int canBuy,  int capacity , int dp[][][]){
+       if(capacity <=0 || day >= prices.length){
+        return 0;
+       }
+       if(dp[canBuy][capacity][day] != -1 ){
+         return dp[canBuy][capacity][day] ;
+       }
+       if(canBuy == 1 ){
+        return dp[canBuy][capacity][day] =  Math.max(-prices[day]+func(prices, day+1, 0 , capacity , dp), func(prices, day+1, 1, capacity ,dp));
+       }
+       return dp[canBuy][capacity][day] = Math.max( prices[day]+func(prices, day+1, 1, capacity-1 ,dp) , func(prices, day+1 ,0 , capacity ,dp));
+    
+    }
+}
