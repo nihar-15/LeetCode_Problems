797002d980e8ce988196a8c8c38f77b7159e0ce0
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-05-18 09:48:58 +0530

    Time: 1 ms (92.37%), Space: 45.5 MB (64.68%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,11 +2,41 @@ import java.util.Arrays;
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int dp[][] = new int[prices.length][2];
+       
+       /*
+        int n = prices.length;
+        int dp[][] = new int[prices.length+1][2];
         for (int d[] : dp) {
             Arrays.fill(d, -1);
         }
-        return func(prices, 0, 1, dp);
+      int profit = 0;
+
+        dp[n][0] = dp[n][1] = 0;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy == 0) { // We can buy the stock
+                    profit = Math.max(0 + dp[ind + 1][0], -prices[ind] + dp[ind + 1][1]);
+                }
+
+                if (buy == 1) { 
+                    profit = Math.max(0 + dp[ind + 1][1], prices[ind] + dp[ind + 1][0]);
+                }
+
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][0];
+        */
+        int sum =0;
+        for(int i =0;i<prices.length-1;i++){
+            if(prices[i+1] > prices[i]){
+                sum += prices[i+1] - prices[i];
+            }
+
+        }
+
+  return sum;
+       // return func(prices, 0, 1, dp);
     }
 
     int func(int prices[], int day, int canBuy, int dp[][]) {
