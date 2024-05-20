7d4ac0c7d8e9f07eb0d0b57740b812ede42e2840
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-05-20 02:02:02 -0400

    Time: 3 ms (95.18%), Space: 44.1 MB (48.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,35 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-     
-        int n=nums.length;
-       int dp[][]=new int[n][n+1]; 
-        
-         for (int row[] : dp) {
-            Arrays.fill(row, -1);
-        }
 
-       int ans= func(0,-1,nums,dp);
-        
-        return ans;
+       // Aniruddha Agrawal sir
+       int[] dp = new int[nums.length];
+    int max = 0;
+    for(int num : nums) {
+        int index = Arrays.binarySearch(dp, 0, max, num);
+        if(index < 0) index = Math.abs(index) - 1;
+        dp[index] = num;
+        if(index == max) ++max;
     }
     
-    private static int func(int curr,int prev,int[] nums,int dp[][]){
-        
-        if(curr==nums.length){
-            return 0;
-        }
-        
-        if(dp[curr][prev+1]!=-1){
-            return dp[curr][prev+1];
-        }
-
-        
-        int len =0+func(curr+1,prev,nums,dp);
-        
-        if(prev==-1 || nums[curr]> nums[prev]){
-            len=Math.max(len,1+func(curr+1,curr,nums,dp));
-        }
-        
-        return dp[curr][prev+1]=len;
+    return max;
+ 
+       
     }
 }
