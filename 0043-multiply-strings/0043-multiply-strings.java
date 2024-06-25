20596ac21f6c94f095b245a2173c891a7d94c944
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-06-25 12:12:31 -0400

    Time: 2 ms (98.61%), Space: 41.8 MB (91.91%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,9 +1,32 @@
 import java.math.*;
 class Solution {
     public String multiply(String num1, String num2) {
-    BigInteger a = new BigInteger(num1);
-     BigInteger b = new BigInteger(num2);
+    int n1 = num1.length();
+    int n2 = num2.length();
 
-     return a.multiply(b).toString();
+    int products[] = new int[n1+n2];
+
+    for(int i = n1-1;i>=0;i--){
+        for(int j = n2-1 ;j>=0 ;j--){
+            int d1 = num1.charAt(i) - '0';
+            int d2 = num2.charAt(j) - '0';
+           products[i+j+1] += d1*d2;
+        }
+    }
+    int carry =0;
+    for(int i = products.length-1;i>=0;i--){
+        int temp = (carry+ products[i])%10;
+        carry = (carry+products[i])/10;
+        products[i] = temp;
+    }
+    StringBuilder sb = new StringBuilder();
+        for (int num : products) {
+            sb.append(num);
+        }
+ while (sb.length() != 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+
+  return sb.length() == 0 ? "0" : sb.toString();
     }
 }
