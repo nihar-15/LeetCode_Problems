7f5fc8152fde666c162e34fa6cb0521cbf0dd22c
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-06-28 01:01:08 -0400

    Time: 220 ms (40%), Space: 12.3 MB (60%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.go
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.go
@@ -1,18 +1,12 @@
 func maximumImportance(n int, roads [][]int) int64 {
    degree := make([]int, n)
-
    for i:=0 ;i<len(roads) ; i++{
      degree[ roads[i][0]]++
      degree[roads[i][1]]++
    }
-
-   sort.Ints(degree[:])
-
-   var val int64
-   val =1
-   var ans int64
-   ans =0
-
+   sort.Ints(degree)
+   var val int64 =1
+   var ans int64 = 0
    for i:=0;i<len(degree);i++{
      k := val  *  int64(degree[i])
      ans = ans+ k
