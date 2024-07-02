1bce0d0c1cf18f4d0e4d14cd3c96b6969763f3d0
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-02 00:01:48 -0400

    Time: 3 ms (68.73%), Space: 2.8 MB (88.41%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.go
@@ -0,0 +1,19 @@
+func intersect(nums1 []int, nums2 []int) []int {
+    sort.Ints(nums1)
+    sort.Ints(nums2)
+    ans := [] int{}
+    i, j := 0 ,0
+
+    for i < len(nums1) && j < len(nums2){
+        if(nums1[i] == nums2[j]){
+            ans = append(ans, nums1[i])
+            i++
+            j++
+        }else if( nums1[i] < nums2[j]){
+            i++
+        }else{
+            j++
+        }
+    }
+    return ans
+}
