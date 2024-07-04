cb897854331e36b9cb077eb4e6478855ca9c1c41
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-04 05:45:25 -0400

    Time: 8 ms (85.17%), Space: 44.3 MB (75.7%) - LeetHub

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,5 +1,4 @@
 class Solution {
-     private Map<String, Integer> memo = new HashMap<>();
     public int maxLength(List<String> arr) {
         int  n = arr.size();
         String temp = "";
@@ -9,10 +8,6 @@ class Solution {
         if( idx >= n){
             return temp.length();
         }
-         String key = idx + "," + temp;
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
         int include = 0 ,exclude = 0;
         if(hasDuplicates(arr.get(idx) , temp)){
             exclude = func(arr , idx+1, n , temp);
@@ -20,9 +15,7 @@ class Solution {
             exclude = func(arr, idx+1, n , temp);
             include = func(arr , idx+1 , n , temp+arr.get(idx));
         }
-       int result =  Math.max(include , exclude);
-         memo.put(key, result);
-         return result ;
+        return Math.max(include  , exclude);
     }
     boolean hasDuplicates(String s1 , String s2){
         boolean v[] = new boolean[26];
