6737c72915e8fa1743454f63be58e739a09d2906
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-04 13:02:59 -0400

    Time: 65 ms (29.42%), Space: 43.5 MB (25.36%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,10 +1,11 @@
 class Solution {
-    int arr[] = new int[1000001];
+    
     public int numSquares(int n) {
+        int arr[] = new int[n+1];
         Arrays.fill(arr , -1);
-        return func(n);
+        return func(n , arr);
     }
-    int func(int n){
+    int func(int n , int arr[]){
         if(n ==  0 ){
             return 0;
         }
@@ -13,7 +14,7 @@ class Solution {
         }
         int minimumCount = Integer.MAX_VALUE;
         for(int i = 1 ;i*i <= n ; i++){
-            int res = 1+ func(n - i*i);
+            int res = 1+ func(n - i*i , arr);
             minimumCount = Math.min(res , minimumCount);
         }
         return arr[n] = minimumCount;
