f6a07111a219d654b48dcf7005107cb86aaf3d14
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-06 22:55:29 +0530

    Time: 0 ms (100%), Space: 40.7 MB (67.24%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,41 +1,32 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1){
+        if(nums.length==1){
             return nums[0];
         }
-      List<Integer> arr = new ArrayList<>();
-      for (int num : nums) {
-        arr.add(num);
-      }
-       int first = arr.get(0);
-      arr.remove(0);
-       int ans1 = rob2(arr);
-       arr.add(0 , first);
-       int last = arr.get(nums.length-1);
-       arr.remove(nums.length-1);
-       int ans2 = rob2(arr);
-       return Math.max(ans1 , ans2);
+        int [] temparr=new int [nums.length-1];
+        int [] temparr2=new int [nums.length-1];
+        for(int i=0;i<temparr.length;i++){
+            temparr[i]=nums[i];
+            temparr2[i]=nums[i+1];
+        }
+        return Math.max(rob1(temparr),rob1(temparr2));
     }
-     public int rob2(List<Integer> nums) {
-        int dp[] = new int[nums.size()+1];
-        Arrays.fill(dp , -1);
-        return func(nums, 0 , dp);
+    public int rob1(int[] nums) {
+        int [] dp=new int [nums.length];
+        for(int i=0;i<dp.length;i++){
+            dp[i]=-1;
+        }
+        return solve(nums.length-1,nums,dp);
     }
-    int func(List<Integer> nums, int idx , int dp[]){
-        if(idx >= nums.size()){
+    public int solve(int n,int[] nums,int[] dp){
+        if(n<0){
             return 0;
         }
-        if(idx == nums.size() -1){
-            return nums.get(idx);
-        }
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int take = nums.get(idx)+ func(nums, idx+2 , dp);
-        int notTake = Integer.MIN_VALUE;
-        if(idx + 1 < nums.size()){
-             notTake = func(nums , idx + 1 , dp);
-        }
-        return dp[idx] = Math.max(take , notTake);
+
+        int take=(dp[n]!=-1)? dp[n]:nums[n]+solve(n-2,nums,dp);
+        int not=-1;
+        if(n>0)not=(dp[n-1]!=-1)? dp[n-1]:solve(n-1,nums,dp);
+        dp[n]=Math.max(take,not);
+        return Math.max(take,not);
     }
 }
