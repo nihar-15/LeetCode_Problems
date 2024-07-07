a20668866647c63b95424f74bb3ad3eafbcae57c
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-07 14:40:38 +0530

    Time: 2 ms (35.25%), Space: 41.8 MB (34.89%) - LeetHub

---
+++ 1381-maximum-score-words-formed-by-letters/1381-maximum-score-words-formed-by-letters.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        List<List<String>> list = new ArrayList<>();
+        int freq[] = new int[26];
+        for(int i = 0; i < letters.length; i++){
+            freq[letters[i] - 'a']++;
+        }
+        int maxScore = 0 ;
+        return func(list, 0, new ArrayList<>(), freq, words, score, maxScore);
+    }
+
+    int func(List<List<String>> list, int idx, List<String> cur, int freq[], String[] words, int score[], int maxScore) {
+        if(idx == words.length){
+            int currentScore = 0;
+            for(int i = 0; i < cur.size(); i++){
+                for(int j = 0; j < cur.get(i).length(); j++){
+                    currentScore += score[cur.get(i).charAt(j) - 'a'];
+                }
+            }
+            return Math.max(maxScore, currentScore);
+        }
+
+        if(isFeasible(words[idx], freq)){
+            cur.add(words[idx]);
+            reduce(freq, words[idx]);
+            maxScore = func(list, idx + 1, cur, freq, words, score, maxScore);
+            cur.remove(cur.size() - 1);
+            add(freq, words[idx]);
+        }
+
+        return func(list, idx + 1, cur, freq, words, score, maxScore);
+    }
+
+    boolean isFeasible(String word, int freq[]) {
+        int[] tempFreq = Arrays.copyOf(freq, freq.length);
+        for(int i = 0; i < word.length(); i++){
+            if(--tempFreq[word.charAt(i) - 'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void reduce(int freq[], String word) {
+        for(int i = 0; i < word.length(); i++){
+            freq[word.charAt(i) - 'a']--;
+        }
+    }
+
+    void add(int freq[], String word) {
+        for(int i = 0; i < word.length(); i++){
+            freq[word.charAt(i) - 'a']++;
+        }
+    }
+}
