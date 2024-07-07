022c376ad55e9ca43c129c7b468684994b0c26af
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-07 20:56:49 +0530

    Time: 2 ms (22.3%), Space: 42.8 MB (5.38%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,26 @@
+class Solution {
+    int dp[] = new int[1001];
+    public int numDecodings(String s) {
+        int n =  s.length();
+        Arrays.fill(dp , -1);
+         return func(0 , n , s);
+    }
+    int func(int i , int n , String s){
+        if( i == n){
+            return 1;
+        }
+        if(s.charAt(i) == '0'){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        int result = func(i+1 , n , s);
+        if(i+1 < n){
+            if(s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i+1) <= '6'){
+                result += func(i+2 , n , s);
+            }
+        }
+        return dp[i] = result;
+    }
+}
