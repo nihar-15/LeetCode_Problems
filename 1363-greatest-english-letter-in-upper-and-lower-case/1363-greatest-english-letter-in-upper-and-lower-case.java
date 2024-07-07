2ca5dc7048bdf517195bca9b9ce958f9e34526db
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-07 21:48:57 +0530

    Time: 0 ms (100%), Space: 41.5 MB (94.92%) - LeetHub

--- 1363-greatest-english-letter-in-upper-and-lower-case/1363-greatest-english-letter-in-upper-and-lower-case.java
+++ 1363-greatest-english-letter-in-upper-and-lower-case/1363-greatest-english-letter-in-upper-and-lower-case.java
@@ -1,14 +1,13 @@
 class Solution {
     public String greatestLetter(String s) {
-        
-        Set<Character> set = new HashSet<>();
-        for(char ch : s.toCharArray())
-            set.add(ch);
-        
-        for(char ch = 'Z'; ch >= 'A'; ch--)
-            if(set.contains(ch) && set.contains((char)('a'+(ch-'A'))))
-               return ""+ch;
-        return "";
+        for (int i = 'z'; i >= 'a'; i--) {
+            String lower = Character.toString(i);
+            String upper = Character.toString(i - 32);
+            if (s.contains(lower) && s.contains(upper)) {
+                return upper;
+            }
+        }
 
+        return "";
     }
 }
