1e41927d121ae1567eb31e1183f5d435f094f620
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-07 18:07:09 +0530

    Time: 7 ms (54.21%), Space: 45.3 MB (27.56%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,76 +1,31 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-     return func2(matrix);
-    /*   int n = matrix.length;
-       int m = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
 
-         int dp[][]= new   int[n+1][m+1];
-       for(int[] a : dp){
-        Arrays.fill(a ,-1);
-       }
-       int mini =(int) 1e9;
-       for(int i=0;i<m;i++){
-          int ans = func(matrix, 0 , i, dp);
-           mini = Math.min(mini , ans);
-     }
-
-        return mini;
-        */
-  
-    }
-    /*
-    This is written my me And gives TLE
-    int func(int [][]matrix, int i ,int j , int dp[][]){
-        if(j>= matrix[0].length || j<0){
-            return (int)1e9;
-        }
-        if(i==matrix.length-1){
-            return matrix[i][j];
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int down = matrix[i][j]+func(matrix , i+1,j, dp);
-        int left = matrix[i][j]+func(matrix , i+1, j-1,dp);
-        int right = matrix[i][j]+func(matrix , i+1, j+1,dp);
-
-        return dp[i][j]=Math.min(down , Math.min(left , right));
-    }
-
-
-*/
-
-
-int func2(int matrix[][]){
-     int n = matrix.length;
-        int m = matrix[0].length;
-
-        // Initialize dp array
-        int[][] dp = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            System.arraycopy(matrix[i], 0, dp[i], 0, m);
+        // Copy the last row of matrix to dp
+        for (int j = 0; j < n; j++) {
+            dp[m - 1][j] = matrix[m - 1][j];
         }
 
-        // Bottom-up dynamic programming
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j < m; j++) {
-                int minVal = dp[i + 1][j];
-                if (j > 0) {
-                    minVal = Math.min(minVal, dp[i + 1][j - 1]);
-                }
-                if (j < m - 1) {
-                    minVal = Math.min(minVal, dp[i + 1][j + 1]);
-                }
-                dp[i][j] += minVal;
+        // Fill the dp array from the second last row to the top
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                int down = dp[i + 1][j];
+                int downLeft = j > 0 ? dp[i + 1][j - 1] : Integer.MAX_VALUE;
+                int downRight = j < n - 1 ? dp[i + 1][j + 1] : Integer.MAX_VALUE;
+
+                dp[i][j] = matrix[i][j] + Math.min(down, Math.min(downLeft, downRight));
             }
         }
 
-        // Find the minimum path sum in the first row
-        int minPathSum = Integer.MAX_VALUE;
-        for (int val : dp[0]) {
-            minPathSum = Math.min(minPathSum, val);
+        // Find the minimum value in the first row of dp
+        int ans = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            ans = Math.min(ans, dp[0][j]);
         }
 
-        return minPathSum;
-}
+        return ans;
+    }
 }
