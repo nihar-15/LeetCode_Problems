dd0973f9ececae93325c4140c2ff68296f068fb2
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-08 10:28:12 +0530

    Time: 0 ms (100%), Space: 40.3 MB (73.64%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,25 +1,10 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-     	Queue<Integer> q= new LinkedList<>();
-		for(int i=1;i<=n;i++){
-		    q.add(i);
-		}
-		int i=k;
-
-		while(q.size()!=1){
-
-		    i=k;
-		    while(i!=1){
-		        int a= q.poll();
-		        q.add(a);
-		        i--;
-
-		}
-     	q.remove();
-
-		}
-
-		return q.poll();
-   
+     	 int i = 1;
+        for(int j =2; j<=n;j++){
+            i += k;
+            i = (i-1)%j + 1;
+        }
+        return i;
     }
 }
