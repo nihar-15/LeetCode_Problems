4e8cf5588b5789ca7c3fe9a00be29bd976f9609b
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-15 22:36:39 +0530

    Time: 2 ms (15.49%), Space: 49 MB (5.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,61 +1,24 @@
-import java.util.Arrays;
-
 class Solution {
     public int maxProfit(int[] prices) {
-       
-       /*
-        int n = prices.length;
-        int dp[][] = new int[prices.length+1][2];
-        for (int d[] : dp) {
-            Arrays.fill(d, -1);
+        int dp[][] = new int[2][prices.length];
+        for(int d[] : dp){
+            Arrays.fill(d , -1);
         }
-      int profit = 0;
-
-        dp[n][0] = dp[n][1] = 0;
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                if (buy == 0) { // We can buy the stock
-                    profit = Math.max(0 + dp[ind + 1][0], -prices[ind] + dp[ind + 1][1]);
-                }
-
-                if (buy == 1) { 
-                    profit = Math.max(0 + dp[ind + 1][1], prices[ind] + dp[ind + 1][0]);
-                }
-
-                dp[ind][buy] = profit;
-            }
-        }
-        return dp[0][0];
-        */
-        int sum =0;
-        for(int i =0;i<prices.length-1;i++){
-            if(prices[i+1] > prices[i]){
-                sum += prices[i+1] - prices[i];
-            }
-
-        }
-
-  return sum;
-       // return func(prices, 0, 1, dp);
+        return func(prices , 0 , 1 , dp);
     }
-
-    int func(int prices[], int day, int canBuy, int dp[][]) {
-        if (day == prices.length) {
+    int func(int []prices , int idx , int canBuy , int dp[][]){
+        if( idx == prices.length){
             return 0;
         }
-
-        if (dp[day][canBuy] != -1) {
-            return dp[day][canBuy];
+        int profit =0  ; 
+        if(dp[canBuy][idx] != -1){
+            return dp[canBuy][idx];
         }
-
-        int profit = 0;
-        if (canBuy == 1) {
-            profit = Math.max(-prices[day] + func(prices, day + 1, 0, dp), func(prices, day + 1, 1, dp));
-        } else {
-            profit = Math.max(prices[day] + func(prices, day + 1, 1, dp), func(prices, day + 1, 0, dp));
+        if( canBuy == 1){
+            profit = Math.max(-prices[idx] + func(prices , idx+1 , 0 , dp) , 0+ func(prices , idx+1 , 1, dp));
+        }else{
+            profit = Math.max(prices[idx] + func(prices , idx+1 , 1 , dp) , func(prices , idx+1 , 0 , dp));
         }
-
-        dp[day][canBuy] = profit;
-        return profit;
+        return dp[canBuy][idx] = profit;
     }
 }
