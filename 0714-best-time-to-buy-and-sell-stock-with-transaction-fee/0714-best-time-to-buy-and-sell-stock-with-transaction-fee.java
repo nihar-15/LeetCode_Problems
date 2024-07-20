5b3e63403dbc4427d3555272501e88a284538e39
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-20 14:43:05 +0530

    Time: 18 ms (53.18%), Space: 55.4 MB (35.75%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,22 +1,34 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int dp[][] = new int[prices.length][2];
-        for(int d[]: dp){
+        int dp[][] = new int[prices.length+1][2];
+       /* for(int d[]: dp){
             Arrays.fill(d , -1);
         }
-        return  func(prices ,0 , 1, fee ,dp);
+        */
+      for(int day =  prices.length-1 ;day>=0; day -- ){
+        for(int canBuy=0;canBuy<=1; canBuy++){
+            if(canBuy == 1){
+                dp[day][canBuy] = Math.max(-prices[day] + dp[day+1][0] , dp[day+1][1]);
+            }else{
+                dp[day][canBuy]  =    Math.max( prices[day]+dp[day+1][1]-fee , dp[day+1][0]);
+            }
+        }
+      }
+
+   return dp[0][1];
+        //return  func(prices ,0 , 1, fee ,dp);
     }
     int func(int prices[] , int day , int canBuy , int fee , int dp[][]){
        if(prices.length == day){
         return 0;
        }
-       if(dp[day][canBuy] != -1){
-        return dp[day][canBuy];
+       if(dp[canBuy][day]  != -1){
+        return dp[canBuy][day] ;
        }
         if(canBuy == 1){
-            return dp[day][canBuy] = Math.max(-prices[day] + func(prices, day+1 , 0 , fee ,dp) , func(prices, day+1 ,1, fee ,dp));
+            return dp[canBuy][day] = Math.max(-prices[day] + func(prices, day+1 , 0 , fee ,dp) , func(prices, day+1 ,1, fee ,dp));
         }
-        return dp[day][canBuy] = Math.max( prices[day]+func(prices , day+1 , 1 , fee, dp)-fee , func(prices , day+1 , 0 , fee, dp));
+        return dp[canBuy][day] = Math.max( prices[day]+func(prices , day+1 , 1 , fee, dp)-fee , func(prices , day+1 , 0 , fee, dp));
     }
 
 }
