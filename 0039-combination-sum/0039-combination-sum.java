2cc9a675fe5924d5580c514957043d67e7c0918a
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-08-14 00:54:41 +0530

    Time: 2 ms (84.16%), Space: 44.4 MB (65.44%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
+     Arrays.sort(candidates);
         List<List<Integer>> ans = new ArrayList<>();
         func(candidates , target , 0 ,  ans , new ArrayList<>());
         return ans; 
