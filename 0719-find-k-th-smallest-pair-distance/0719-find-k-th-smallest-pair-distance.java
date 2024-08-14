590f45f67793653a5e4464ccdaa8e60ae3ea7ce2
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-08-15 00:36:39 +0530

    Time: 7 ms (96.26%), Space: 44 MB (89.46%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -12,11 +12,11 @@ class Solution {
      }   
      return pq.poll();
      */
-       int n = nums.length;
+      /* int n = nums.length;
         int maxEl = Arrays.stream(nums).max().getAsInt();
         int[] vec = new int[maxEl + 1];
 
-        // All pairs
+        
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 int d = Math.abs(nums[i] - nums[j]);
@@ -31,5 +31,34 @@ class Solution {
             }
         }
         return -1;
+        */
+        Arrays.sort(nums);
+        int n = nums.length ;
+        int result = 0;
+        int low = 0 , high = nums[n-1] - nums[0];
+        while(low <= high){
+         int mid = (low + high ) / 2;
+         if(countOfPairs(nums , mid) < k){
+            
+            low = mid+1;
+         }else{
+            result = mid ;
+            high = mid - 1;
+         }
+        }
+      return result ;
+    }
+    int countOfPairs(int nums[] , int dis){
+        int i =0 , j = 0;
+        int n = nums.length;
+        int pairs = 0;
+        while( j < n ){
+            while(nums[j] - nums[i] > dis){
+                i++;
+            }
+            pairs += j -i;
+            j++;
+        }
+        return pairs;
     }
 }
