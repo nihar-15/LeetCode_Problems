6f02b1a41134b990d1f4d527f8e390139cd011a1
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-08-14 16:39:39 +0530

    Time: 1337 ms (5.02%), Space: 133.4 MB (40.44%) - LeetHub

--- 3486-count-the-number-of-good-nodes/3486-count-the-number-of-good-nodes.java
+++ 3486-count-the-number-of-good-nodes/3486-count-the-number-of-good-nodes.java
@@ -1,46 +1,32 @@
 class Solution {
-    int cnt = 0;
-
+    int cnt = 0 ;
     public int countGoodNodes(int[][] edges) {
-        int n = edges.length + 1;  // Number of nodes
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        List<List<Integer>>  adj = new ArrayList<>();
+        for(int i =0 ; i<=edges.length ; i++){
             adj.add(new ArrayList<>());
         }
-
-        // Build undirected graph
-        for (int[] edge : edges) {
+        for(int []edge : edges){
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
-
-        dfs(adj, 0, -1);
-        return cnt;
+      
+       dfs(adj , 0 , -1);
+        return cnt ;
     }
-
-    int dfs(List<List<Integer>> adj, int node, int parent) {
-        int size = 1;
-        int firstSubtreeSize = -1;
-
-        boolean isGood = true;
-        for (int neighbor : adj.get(node)) {
-            if (neighbor != parent) {
-                int subtreeSize = dfs(adj, neighbor, node);
-                size += subtreeSize;
-
-                if (firstSubtreeSize == -1) {
-                    firstSubtreeSize = subtreeSize;
-                } else if (firstSubtreeSize != subtreeSize) {
-                    isGood = false;
-                }
+    int dfs(List<List<Integer>> adj , int node , int parent){
+         int size =1 ; 
+         Set<Integer> set = new HashSet<>();
+         for(int a : adj.get(node)){
+            if( a!= parent){
+                int leaf = dfs(adj , a , node);
+                size += leaf;
+                set.add(leaf);
             }
-        }
-
-        
-        if (isGood) {
-            cnt++;
-        }
-
-        return size;
+         }
+      if(set.size() <= 1){
+        System.out.println(node);
+        cnt++;
+      }
+      return size;
     }
 }
