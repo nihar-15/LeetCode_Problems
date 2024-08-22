4850fe0f67e2f0517aeb4a6a4c32709a0567012c
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-08-23 00:34:29 +0530

    Time: 12 ms (91.03%), Space: 57.8 MB (41.95%) - LeetHub

--- 2725-mice-and-cheese/2725-mice-and-cheese.java
+++ 2725-mice-and-cheese/2725-mice-and-cheese.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 
 class Solution {
     public int miceAndCheese(int[] reward1, int[] reward2, int k) {
@@ -6,18 +6,14 @@ class Solution {
         int[] diff = new int[n];
         int ans = 0;
 
-        // Calculate the difference between reward1 and reward2
         for (int i = 0; i < n; i++) {
             diff[i] = reward1[i] - reward2[i];
-            ans += reward2[i]; // Initially add all rewards from reward2
+            ans += reward2[i]; 
         }
-
-        // Sort the differences in descending order
         Arrays.sort(diff);
 
-        // Add the top k differences to the result
         for (int i = 0; i < k; i++) {
-            ans += diff[n - 1 - i]; // Picking the k largest differences
+            ans += diff[n - 1 - i]; 
         }
 
         return ans;
