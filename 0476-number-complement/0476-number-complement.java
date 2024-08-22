5f92768cacd60893c3e4f61b330a1535a0d01610
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-08-22 22:46:29 +0530

    Time: 2 ms (12.59%), Space: 40.8 MB (12.78%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,8 +1,6 @@
 class Solution {
     public int findComplement(int num) {
 
-        class Solution {
-    public int findComplement(int num) {
         String str = Integer.toBinaryString(num);
         String res = "";
         for(int i=0;i<str.length();i++){
