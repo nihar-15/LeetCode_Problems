dfcea712c96734e954e5ea12d9f4d84a00588603
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-08-22 22:46:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,5 +1,22 @@
 class Solution {
     public int findComplement(int num) {
+
+        class Solution {
+    public int findComplement(int num) {
+        String str = Integer.toBinaryString(num);
+        String res = "";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '0'){
+                res += '1';
+            }
+            else{
+                res += '0';
+            }
+        }
+        return Integer.parseInt(res,2);
+    }
+}
+/*
         String binaryString = getBinary(num);
         int number = Integer.parseInt(binaryString, 2);
         return number;
@@ -19,3 +36,5 @@ class Solution {
     }
 
 }
+
+*/
