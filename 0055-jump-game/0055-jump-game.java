2da3945dd5fec924485e7d4e9bfc7047f9cc9d5d
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-08-25 12:10:40 +0530

    Time: 336 ms (7.85%), Space: 46.2 MB (5.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean canJump(int[] nums) {
-
-
      int dp[] = new int[nums.length+1];
      Arrays.fill(dp ,-1);
       return func(nums,0 ,dp);
@@ -15,12 +13,12 @@ class Solution {
     }
 
     if (dp[idx] != -1) {
-        return dp[idx] == 1; // If dp[idx] is not -1, return true if dp[idx] is 1, otherwise false
+        return dp[idx] == 1; 
     }
 
     int maxReach = Math.min(idx + nums[idx], nums.length - 1);
 
-    // Traverse through all possible steps from current position
+   
     for (int i = idx + 1; i <= maxReach; i++) {
         if (func(nums, i, dp)) {
             dp[idx] = 1; // Assign 1 to indicate true
