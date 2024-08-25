a67dff02319720567e8247b71d60d2b2bd252b22
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-08-25 12:22:09 +0530

    Time: 120 ms (8.36%), Space: 45.9 MB (5.54%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,23 +1,22 @@
 class Solution {
     public int jump(int[] arr) {
-          int n = arr.length+1;
+        int n = arr.length+1;
         int dp[]  = new int[n];
         Arrays.fill(dp , -1);
-        return func(arr , 0 , 0 , dp);
+        return func(arr , 0  , dp);
     }
-     static int func(int arr[] , int idx , int  steps ,int dp[]){
-     //  System.out.println(steps);
-    
-        if(idx == arr.length-1){
+     static int func(int arr[] , int idx , int dp[]){
+        
+        if(idx == arr.length -1){
             return 0;
         }
-        int minSteps = Integer.MAX_VALUE - 1; 
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        for(int jumps =1;jumps<=arr[idx];jumps++){
-            if(idx+jumps < arr.length){
-            minSteps = Math.min(minSteps , func(arr , idx+ jumps, steps ,dp)+1);
+        int minSteps = Integer.MAX_VALUE -1;
+       if(dp[idx] != -1){
+        return dp[idx];
+       }
+        for(int i = 1 ; i<= arr[idx] ; i++){
+            if(idx + i < arr.length){
+                minSteps = Math.min(minSteps , func(arr, idx + i, dp) +1);
             }
         }
         return dp[idx] = minSteps;
