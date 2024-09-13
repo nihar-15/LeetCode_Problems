96867881fbe5b257fde541a3fe263c6474dab38d
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-09-13 08:14:07 +0530

    Time: 547 ms (15.78%), Space: 58.3 MB (28.07%) - LeetHub

---
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int res[] = new int[queries.length];
+        int i = 0;
+        for (int[] query : queries) {
+            int left = query[0];
+            int right = query[1];
+            int xorOfElem = arr[left];
+
+            while (left < right) {
+                left++;
+                xorOfElem ^= arr[left];
+            }
+
+            res[i++] = xorOfElem;
+        }
+        return res;
+    }
+}
