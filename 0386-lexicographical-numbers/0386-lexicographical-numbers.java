397b4538fc97c20979508e0345775741d7a5dea7
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-09-22 22:45:16 +0530

    Time: 20 ms (17.74%), Space: 51.3 MB (9.02%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,32 +1,14 @@
 class Solution {
-       private void solve(int curr, int n, List<Integer> result) {
-        if (curr > n) {
-            return;
-        }
-        
-        result.add(curr);  // Add current number to result
-        
-        // Generate the next number by appending digits 0 to 9
-        for (int nextDigit = 0; nextDigit <= 9; nextDigit++) {
-            int nextNum = curr * 10 + nextDigit;
-            
-            if (nextNum > n) {
-                return;
-            }
-            
-            solve(nextNum, n, result);  // Recursive call
-        }
-    }
-
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> result = new ArrayList<>();
-        
-        // Start from numbers 1 to 9
-        for (int num = 1; num <= 9; num++) {
-            solve(num, n, result);
+        List<String> a = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i =1 ; i <= n; i++){
+           a.add(Integer.toString(i));
         }
-        
-        return result; 
-        
+        Collections.sort(a);
+        for(String num : a){
+            ans.add(Integer.parseInt(num));
+        }
+  return ans;
     }
 }
