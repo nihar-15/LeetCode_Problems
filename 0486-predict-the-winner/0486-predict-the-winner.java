408580b8aa6eeff8541009df3be98aa6912fc888
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-09-23 12:41:45 +0530

    Time: 71 ms (19.85%), Space: 41.3 MB (20.66%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,40 +1,23 @@
 class Solution {
-    public boolean predictTheWinner(int[] piles) {
-       int n = piles.length;
-       if(n == 1){
-        return true;
-       }
-        int total = 0 ; 
-        int dp[][][] = new int[piles.length][piles.length][2];
-        for(int d[][] : dp){
-            for(int a[] : d){
-                Arrays.fill(a , -1);
-            }
+    public boolean predictTheWinner(int[] nums) {
+        int sum =0 ;
+        int n  = nums.length ;
+        for(int num : nums){
+            sum += num ;
         }
-        for(int num : piles){
-            total += num;
-        }
-        if(total == 0){
-            return true;
-        }
-        int maxi = func(piles , 0 , n -1 , 0 , dp);
-        return (maxi >= (total - maxi));
+        int player1 = func(nums , 0 , n-1);
+        int player2 = sum - player1;
+        return player1 >= player2;
     }
-    int func(int piles[] , int left , int right , int turn , int dp[][][]){
-       if(left > right){
-        return 0;
-       }
-        int ans = 0 ;
-        if(dp[left][right][turn] != -1){
-            return dp[left][right][turn];
+    int func(int nums[] , int i , int j ){
+        if(i > j){
+            return 0;
         }
-        if(turn == 0){
-            int current = Math.max(piles[left] + func(piles , left+ 1 , right , 1 ,dp) , piles[right] + func(piles , left , right -1  , 1 , dp));
-            ans = Math.max(current , ans);
-        }else{
-            int current = Math.min(func(piles , left+1 , right , 0 , dp) , func(piles , left , right -1 , 0 ,dp));
-            ans = Math.max(current , ans);
+        if(i == j){
+            return nums[i];
         }
-        return dp[left][right][turn] =  ans ;
+        int takeIth = nums[i] + Math.min(func(nums , i+2 ,j) , func(nums , i +1 , j-1));
+        int takeJth = nums[j] + Math.min(func(nums , i , j -2) , func(nums , i+1  , j -1));
+        return Math.max( takeIth , takeJth);
     }
 }
