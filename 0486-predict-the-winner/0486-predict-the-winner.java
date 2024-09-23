d07cb141df1480016ad4cdf769c2f6cebe280fc5
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-09-23 12:44:37 +0530

    Time: 0 ms (100%), Space: 40.9 MB (50.56%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,6 +1,10 @@
 class Solution {
+    int dp[][] = new int[23][23];
     public boolean predictTheWinner(int[] nums) {
         int sum =0 ;
+        for(int d[] : dp){
+            Arrays.fill(d , -1);
+        }
         int n  = nums.length ;
         for(int num : nums){
             sum += num ;
@@ -13,11 +17,14 @@ class Solution {
         if(i > j){
             return 0;
         }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
         if(i == j){
             return nums[i];
         }
         int takeIth = nums[i] + Math.min(func(nums , i+2 ,j) , func(nums , i +1 , j-1));
         int takeJth = nums[j] + Math.min(func(nums , i , j -2) , func(nums , i+1  , j -1));
-        return Math.max( takeIth , takeJth);
+        return dp[i][j] = Math.max( takeIth , takeJth);
     }
 }
