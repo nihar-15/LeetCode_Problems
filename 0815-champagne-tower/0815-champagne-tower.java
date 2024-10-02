6cddcbbe5e64ba68b8a614d0e309f6b50315abd0
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-10-02 09:59:45 +0530

    Time: 309 ms (6.58%), Space: 62.7 MB (7.24%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.java
+++ 0815-champagne-tower/0815-champagne-tower.java
@@ -1,30 +1,30 @@
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        double dp[][] = new double[1001][1001];
+      double dp[][] = new double[1001][1001];
         for(double d[] : dp){
             Arrays.fill(d , -1);
         }
         return Math.min(1.0 ,func(poured , query_row , query_glass ,dp));
     }
-    double func(int poured , int i , int j  ,double[][] dp){
-        if(i < 0 || j > i || j < 0)
-            return 0.0;
+    double func(int poured , int i , int j , double[][] dp){
 
-        if(i == 0 && j == 0){
-            return (double)poured;
+        if(i < 0 || j < 0 || j > i){
+            return 0;
         }
-        if(dp[i][j] != -1){
+        if(i == 0 && j == 0){
+            return poured;
+        }
+          if(dp[i][j] != -1){
             return dp[i][j];
         } 
-      double a = (func(poured ,i-1 , j-1  , dp) -1) /2.0;
-      double b = (func(poured  , i-1 , j  , dp) -1) /2.0;
-      if(a < 0){
-        a = 0.0;
-      } 
-      if(b < 0){
-        b = 0.0 ;
-      }
-
-       return dp[i][j] = a + b; 
+        double up_left = (func(poured , i-1 , j-1 , dp) - 1)/ 2.0;
+        double up_right = (func(poured , i-1 , j , dp) - 1)/ 2.0;
+       if(up_left < 0){
+         up_left = 0;
+       }
+       if(up_right < 0){
+        up_right = 0;
+       }
+       return dp[i][j] = up_left + up_right ;
     }
 }
