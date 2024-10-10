4b350aca396ebf7d7a3926511258f1b54904252f
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-10-10 11:33:27 +0530

    Time: 27 ms (36.82%), Space: 54.6 MB (45.27%) - LeetHub

---
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -0,0 +1,16 @@
+
+class
+Solution {
+    public int maxWidthRamp(int[] nums) {
+        Stack<Integer> st=new Stack<>();
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(st.isEmpty()||nums[st.peek()]>nums[i]) st.push(i);
+        }
+        for(int i=nums.length-1;i>=0;i--){
+            while(!st.isEmpty()&&nums[st.peek()]<=nums[i])
+                ans=Math.max(ans,i-st.pop());
+        }
+        return ans;
+    }
+}
