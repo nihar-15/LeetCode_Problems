d07e4329e0c42bd3c1ab56054eab8a18d2ce6210
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-10-21 08:58:46 +0530

    Time: 36 ms (43.14%), Space: 44.8 MB (72.55%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.java
@@ -7,20 +7,15 @@ class Solution {
         if (start == s.length()) {
             return 0; 
         }
-        
-        int maxSplits = 0;
-        
-        for (int end = start + 1; end <= s.length(); end++) {
-            String subStr = s.substring(start, end);
-            
-            if (!set.contains(subStr)) {
+        int maxSplits = 0 ; 
+        for(int end = start  + 1; end <= s.length() ; end++){
+            String subStr = s.substring(start , end);
+             if(!set.contains(subStr)){
                 set.add(subStr);
-                maxSplits = Math.max(maxSplits, 1 + func(s, set, end));
-                
-                set.remove(subStr); 
-            }
+                maxSplits = Math.max(maxSplits , 1 + func(s , set , end));
+                set.remove(subStr);
+             }
         }
-        
-        return maxSplits; 
+        return maxSplits;
     }
 }
