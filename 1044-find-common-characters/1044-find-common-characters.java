dead1fdde5078bff15e15f9cd1d53907e05f6158
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-11-03 16:43:49 +0530

    Time: 19 ms (15.4%), Space: 44.3 MB (29.26%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.java
+++ 1044-find-common-characters/1044-find-common-characters.java
@@ -2,66 +2,37 @@ import java.util.*;
 
 class Solution {
     public List<String> commonChars(String[] words) {
-        for (int i = 0; i < words.length; i++) {
-            char[] ar = words[i].toCharArray();
-            Arrays.sort(ar);
-            words[i] = new String(ar);
+     Map<Character,Integer>hmap=new HashMap<>();
+     String str=words[0];
+        for(int i=0;i<str.length();i++){
+            char c=str.charAt(i);
+            hmap.put(c,hmap.getOrDefault(c,0)+1);    
         }
-
-        // Using words.length instead of words.length-1 in the loop condition
-        String ans = words[0];
-        for (int j = 1; j < words.length; j++) {
-            ans = lcs(ans, words[j]);
-        }
-
-        // Convert the result string to the list of characters
-        List<String> result = new ArrayList<>();
-        for (char c : ans.toCharArray()) {
-            result.add(String.valueOf(c));
-        }
-        return result;
-    }
-
-    public String lcs(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-
-        int dp[][] = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 0;
-        }
-        for (int i = 0; i <= m; i++) {
-            dp[0][i] = 0;
-        }
-
-        for (int ind1 = 1; ind1 <= n; ind1++) {
-            for (int ind2 = 1; ind2 <= m; ind2++) {
-                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1))
-                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
-                else
-                    dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+        for(int i=1;i<words.length;i++){
+            String newstr=words[i];
+            Map<Character,Integer>freqmap=new HashMap<>();
+            for(int j=0;j<newstr.length();j++){
+                char c=newstr.charAt(j);
+                freqmap.put(c,freqmap.getOrDefault(c,0)+1);
+            }
+            for(Character key:hmap.keySet()){
+                if(freqmap.containsKey(key)){
+                    hmap.put(key,Math.min(hmap.get(key),freqmap.get(key)));
+                }
+                else{
+                    hmap.put(key,0);
+                }
             }
         }
-
-        int len = dp[n][m];
-        int i = n;
-        int j = m;
-
-        int index = len - 1;
-        char[] str = new char[len];
-        while (i > 0 && j > 0) {
-            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                str[index] = s1.charAt(i - 1);
-                index--;
-                i--;
-                j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                i--;
-            } else {
-                j--;
+        List<String> list=new ArrayList<>();
+        for(Character key:hmap.keySet()){
+            if(hmap.get(key)>0){
+                for(int i=0;i<hmap.get(key);i++){
+                    list.add(key+"");
+                }
             }
         }
-        return new String(str);
+        return list;
     }
 
 }
