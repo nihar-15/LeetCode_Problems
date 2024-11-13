d6ca975587af6050b148c201496accd91cefaf5e
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-11-13 20:56:45 +0530

    Time: 0 ms (100%), Space: 7.3 MB (10.79%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.go
+++ 2058-concatenation-of-array/2058-concatenation-of-array.go
@@ -1,11 +1,7 @@
 func getConcatenation(nums []int) []int {
-    ans := make([]int , 2*len(nums))
-    for i := 0;i < len(nums);i++{
-        ans[i] = nums[i]
-    }
-
-     for i := len(nums);i < len(ans);i++{
-        ans[i] = nums[i-len(nums)]
+        ans := make([]int, len(nums) * 2)
+    for i := 0; i < len(ans); i++ {
+        ans[i] = nums[i % len(nums)]
     }
     return ans
 }
