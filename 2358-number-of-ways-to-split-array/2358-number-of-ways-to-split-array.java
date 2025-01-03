2250d4405a72c1cfc3f54d7ff5eabfb7ef97f630
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2025-01-03 18:39:30 +0530

    Time: 3 ms (59.69%), Space: 58.5 MB (55.4%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -1,18 +1,18 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long leftsum = 0 , rightsum = 0 ; 
-        for(int num : nums){
-            rightsum += num ;
+        long leftsum = 0, rightsum = 0;
+        for (int num : nums) {
+            rightsum += num;
         }
         int count = 0;
-        for(int i = 0 ; i < nums.length - 1 ; i++){
+        for (int i = 0; i < nums.length - 1; i++) {
             leftsum += nums[i];
             rightsum -= nums[i];
-           if(leftsum >= rightsum){
-            count++;
+            if (leftsum >= rightsum) {
+                count++;
+            }
         }
-        }
-      
+
         return count;
     }
 }
