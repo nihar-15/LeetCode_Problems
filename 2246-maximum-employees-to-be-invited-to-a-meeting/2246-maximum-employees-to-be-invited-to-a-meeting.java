ef8eafa6a603e5a3696bc044f7303eacddea3873
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2025-01-26 16:34:27 +0530

    Time: 95 ms (15.13%), Space: 76.3 MB (50%) - LeetHub

---
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/2246-maximum-employees-to-be-invited-to-a-meeting.java
@@ -0,0 +1,69 @@
+class Solution {
+   public int BFS(int start, Map<Integer, List<Integer>> adj, boolean[] visited) {
+        Queue<int[]> queue = new LinkedList<>(); 
+        queue.add(new int[]{start, 0});
+        int maxDistance = 0;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int currNode = current[0];
+            int dist = current[1];
+
+            for (int neighbor : adj.getOrDefault(currNode, new ArrayList<>())) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(new int[]{neighbor, dist + 1});
+                    maxDistance = Math.max(maxDistance, dist + 1);
+                }
+            }
+        }
+
+        return maxDistance;
+    }
+
+    public int maximumInvitations(int[] favorite) {
+        int n = favorite.length;
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.computeIfAbsent(favorite[i], k -> new ArrayList<>()).add(i);
+        }
+
+        int longestCycleEmplCount = 0;
+        int happyCoupleEmplCount = 0;
+
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                Map<Integer, Integer> mp = new HashMap<>();
+                int currNode = i;
+                int currNodeCount = 0;
+
+                while (!visited[currNode]) {
+                    visited[currNode] = true;
+                    mp.put(currNode, currNodeCount);
+
+                    int nextNode = favorite[currNode]; 
+                    currNodeCount++;
+
+                    if (mp.containsKey(nextNode)) { 
+                        int cycleLength = currNodeCount - mp.get(nextNode);
+                        longestCycleEmplCount = Math.max(longestCycleEmplCount, cycleLength);
+
+                        if (cycleLength == 2) { // Happy couple case
+                            boolean[] visitedNodes = new boolean[n];
+                            visitedNodes[currNode] = true;
+                            visitedNodes[nextNode] = true;
+                            happyCoupleEmplCount += 2 + BFS(currNode, adj, visitedNodes) + BFS(nextNode, adj, visitedNodes);
+                        }
+                        break;
+                    }
+                    currNode = nextNode;
+                }
+            }
+        }
+
+        return Math.max(happyCoupleEmplCount, longestCycleEmplCount);
+    }
+}
